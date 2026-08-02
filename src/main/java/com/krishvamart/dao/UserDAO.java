f8222ed369dc66a906967077f69f0d42fcb2310e
hayakrishva4 <hayakrishvaa410@gmail.com>
2026-08-02 21:41:14 +0530

    docs: update UserDAO.java

--- src/main/java/com/krishvamart/dao/UserDAO.java
+++ src/main/java/com/krishvamart/dao/UserDAO.java
@@ -1,13 +1,11 @@
 package com.krishva.krishvamart.dao;
-
 import com.krishva.krishvamart.exception.DataAccessException;
 import com.krishva.krishvamart.model.User;
-
 import java.util.Optional;
-
-public interface UserDAO {
+ public interface UserDAO 
+  {
     User insert(User user) throws DataAccessException;
     Optional<User> findById(long id) throws DataAccessException;
     Optional<User> findByEmail(String email) throws DataAccessException;
     java.util.List<User> findAll() throws DataAccessException;
-}
+  }
