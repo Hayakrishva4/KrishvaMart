fe045209ae979f974c03b5c8a5c8bfd54fd51b73
hayakrishva4 <hayakrishvaa410@gmail.com>
2026-08-06 22:58:23 +0530

    docs: ServiceRegistry.java

--- src/main/java/com/krishvamart/listener/ServiceRegistry.java
+++ src/main/java/com/krishvamart/listener/ServiceRegistry.java
@@ -1,12 +1,12 @@
 package com.krishva.krishvamart.listener;
 //import com.krishva.krishvamart.dao.CartDAO;
 //import com.krishva.krishvamart.dao.OrderDAO;
-//import com.krishva.krishvamart.dao.ProductDAO;
+import com.krishva.krishvamart.dao.ProductDAO;
 //import com.krishva.krishvamart.dao.ReviewDAO;
 import com.krishva.krishvamart.dao.UserDAO;
 //import com.krishva.krishvamart.dao.impl.JdbcCartDAO;
 //import com.krishva.krishvamart.dao.impl.JdbcOrderDAO;
-//import com.krishva.krishvamart.dao.impl.JdbcProductDAO;
+import com.krishva.krishvamart.dao.impl.JdbcProductDAO;
 //import com.krishva.krishvamart.dao.impl.JdbcReviewDAO;
 import com.krishva.krishvamart.dao.impl.JdbcUserDAO;
 //import com.krishva.krishvamart.chat.ChatProvider;
@@ -15,15 +15,15 @@ import com.krishva.krishvamart.dao.impl.JdbcUserDAO;
 //import com.krishva.krishvamart.chat.MockChatProvider;
 //import com.krishva.krishvamart.service.CartService;
 //import com.krishva.krishvamart.service.OrderService;
-//import com.krishva.krishvamart.service.ProductService;
+import com.krishva.krishvamart.service.ProductService;
 //import com.krishva.krishvamart.service.ReviewService;
 import com.krishva.krishvamart.service.UserService;
 import javax.sql.DataSource;
 public final class ServiceRegistry {
     public static final String ATTR = "krishvamart.services";
     private final UserService userService;
-   /* private final ProductService productService;
-    private final CartService cartService;
+    private final ProductService productService;
+    /*private final CartService cartService;
     private final OrderService orderService;
     private final ReviewService reviewService;
     private final ChatService chatService;*/
@@ -32,13 +32,13 @@ public final class ServiceRegistry {
     }
     public ServiceRegistry(DataSource dataSource, java.util.Properties chatConfig) {
         UserDAO userDAO = new JdbcUserDAO(dataSource);
-        /*ProductDAO productDAO = new JdbcProductDAO(dataSource);
-        CartDAO cartDAO = new JdbcCartDAO(dataSource);
+        ProductDAO productDAO = new JdbcProductDAO(dataSource);
+        /*UCartDAO cartDAO = new JdbcCartDAO(dataSource);
         OrderDAO orderDAO = new JdbcOrderDAO(dataSource);
         ReviewDAO reviewDAO = new JdbcReviewDAO(dataSource);*/
         this.userService = new UserService(userDAO);
-       /* this.productService = new ProductService(productDAO);
-        this.cartService = new CartService(cartDAO, productDAO);
+        this.productService = new ProductService(productDAO);
+        /*this.cartService = new CartService(cartDAO, productDAO);
         this.orderService = new OrderService(dataSource, orderDAO, productDAO, cartDAO);
         this.reviewService = new ReviewService(reviewDAO, orderDAO);
         String providerFlag = chatConfig.getProperty("ai.chatbot.provider", "mock");
@@ -60,10 +60,10 @@ public final class ServiceRegistry {
     public UserService userService() {
         return userService;
     }
-   /* public ProductService productService() {
+   public ProductService productService() {
         return productService;
     }
-    public CartService cartService() {
+    /*public CartService cartService() {
         return cartService;
     }
     public OrderService orderService() {
